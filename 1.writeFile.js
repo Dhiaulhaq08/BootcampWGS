c2f79331e371d5c1334ac44ca77c99714b58d590
Dhiaulhaq <dhiaulhaq11@gmail.com>
2024-11-19 15:42:14 +0700

    tugas pertama

---
+++ 1.writeFile.js
@@ -0,0 +1,4 @@
+
+const fs = require ('fs');
+
+fs.writeFileSync('text1.txt',"hello world");
---
+++ 3.index.js
@@ -0,0 +1,26 @@
+const readline = require('node:readline');
+const ac = new AbortController();
+const signal = ac.signal;
+//membuat interface agar bisa menampilkan input di cli
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+//membuat pertanyaan yg ditanyakan pada user 
+
+    rl.question('Apa nama kamu? ', (nama) => {
+    console.log(`Nama kamu: ${nama}`);
+      
+    rl.question('Berapa nomor kamu? ', (noTelp) => {
+    console.log(`noTelp kamu: ${noTelp}`);
+        
+    rl.question('Apa email kamu? ', (email) => {
+    console.log(`Hobi kamu: ${email}`);
+          
+          // Menutup readline setelah semua pertanyaan selesai
+          rl.close();
+        });
+      });
+    });
+
+
