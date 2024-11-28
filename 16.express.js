a8ffca734258235e47f942a9117a0c511dd57a92
Dhiaulhaq <dhiaulhaq11@gmail.com>
2024-11-28 14:19:41 +0700

    installing express js and do sendfile in 16.express

--- 16.express.js
+++ 16.express.js
@@ -6,12 +6,15 @@ const path = require('path');
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'index.html'));
 })
-
 app.get('/about', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'about.html'));
 })
 app.get('/contact', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'contact.html'));
+})
+
+app.use((req,res)=> { 
+    res.status(404).send("404 : Not found")
 })
 
 app.listen(3000, ()=> {
